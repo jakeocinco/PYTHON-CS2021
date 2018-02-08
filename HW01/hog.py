a2844cbd97faa31b8f4b9aa6721ffa84bc5a4777
jakeocinco <jakeocinco@me.com>
2018-02-08 16:18:17 -0500

    Seans additions

--- HW01/hog.py
+++ HW01/hog.py
@@ -7,7 +7,7 @@ GOAL_SCORE = 100 # The goal of Hog is to score 100 points.
 ######################
 # Phase 1: Simulator #
 ######################
-d = six_sided;
+
 def roll_dice(num_rolls, dice=six_sided):
     # These assert statements ensure that num_rolls is a positive integer.
     assert type(num_rolls) == int, 'num_rolls must be an integer.'
@@ -37,8 +37,10 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     assert opponent_score < 100, 'The game should be over.'
     # BEGIN Question 2
+    "*** REPLACE THIS LINE ***"
+
     if num_rolls == 0:
-        return max (opponent_score//10, opponent_score % 10) + 1;
+        return max(opponent_score//10, opponent_score % 10) + 1
     return roll_dice(num_rolls, dice)
     # END Question 2
 
@@ -47,9 +49,12 @@ def select_dice(score, opponent_score):
     multiple of 7, in which case select four-sided dice (Hog wild).
     """
     # BEGIN Question 3
+
     if (score + opponent_score) % 7 == 0:
         return four_sided
     return six_sided
+
+    "*** REPLACE THIS LINE ***"
     # END Question 3
 
 def is_swap(score0, score1):
@@ -60,12 +65,13 @@ def is_swap(score0, score1):
     of the last two digits of the second score.
     """
     # BEGIN Question 4
-    l1 = [(score0 // 10) % 10, score0 % 10]
-    l2 = [score1 % 10, (score1 // 10) % 10]
+    "*** REPLACE THIS LINE ***"
 
-    if l1 == l2:
-        return True
+    if (score0 // 10) % 10 == score1 % 10:
+        if score0 % 10 == (score1 // 10) % 10:
+            return True
     return False
+
     # END Question 4
 
 
@@ -79,39 +85,62 @@ def other(who):
     """
     return 1 - who
 
-# def play(strategy0, strategy1, score0=0, score1=0, goal=GOAL_SCORE):
-#     """Simulate a game and return the final scores of both players, with
-#     Player 0's score first, and Player 1's score second.
-#
-#     A strategy is a function that takes two total scores as arguments
-#     (the current player's score, and the opponent's score), and returns a
-#     number of dice that the current player will roll this turn.
-#
-#     strategy0:  The strategy function for Player 0, who plays first
-#     strategy1:  The strategy function for Player 1, who plays second
-#     score0   :  The starting score for Player 0
-#     score1   :  The starting score for Player 1
-#     """
-#     who = 0  # Which player is about to take a turn, 0 (first) or 1 (second)
-#     # BEGIN Question 5
-#     "*** REPLACE THIS LINE ***"
-#     # END Question 5
-#     return score0, score1
-#
-#
-# def always_roll(n):
-#     """Return a strategy that always rolls N dice.
-#
-#     A strategy is a function that takes two total scores as arguments
-#     (the current player's score, and the opponent's score), and returns a
-#     number of dice that the current player will roll this turn.
-#
-#     >>> strategy = always_roll(5)
-#     >>> strategy(0, 0)
-#     5
-#     >>> strategy(99, 99)
-#     5
-#     """
-#     def strategy(score, opponent_score):
-#         return n
-#     return strategy
+def play(strategy0, strategy1, score0=0, score1=0, goal=GOAL_SCORE):
+    """Simulate a game and return the final scores of both players, with
+    Player 0's score first, and Player 1's score second.
+
+    A strategy is a function that takes two total scores as arguments
+    (the current player's score, and the opponent's score), and returns a
+    number of dice that the current player will roll this turn.
+
+    strategy0:  The strategy function for Player 0, who plays first
+    strategy1:  The strategy function for Player 1, who plays second
+    score0   :  The starting score for Player 0
+    score1   :  The starting score for Player 1
+    """
+    who = 0  # Which player is about to take a turn, 0 (first) or 1 (second)
+    # BEGIN Question 5
+    while score0 < goal or score1 < goal: #continue playing game until one player reaches 100
+
+        # IMPLEMENTS THE HOG WILD RULE (I THINK)
+        if select_dice(score0, score1) == four_sided:
+            dice = make_fair_dice(4)
+        else:
+            dice = make_fair_dice(6)
+
+        if who == 0: #Player0 turn
+            # score0 += strategy0
+
+
+
+        else: # player1 turn
+            # score1 += strategy1
+
+        # ALTERNATES TURNS
+        who = other(who)
+
+        # IMPLEMENTS SWINE SWAP RULE
+        if is_swap(score0, score1) == True:
+            score0, score1 = score1, score0
+
+    "*** REPLACE THIS LINE ***"
+    # END Question 5
+    return score0, score1
+
+
+def always_roll(n):
+    """Return a strategy that always rolls N dice.
+
+    A strategy is a function that takes two total scores as arguments
+    (the current player's score, and the opponent's score), and returns a
+    number of dice that the current player will roll this turn.
+
+    >>> strategy = always_roll(5)
+    >>> strategy(0, 0)
+    5
+    >>> strategy(99, 99)
+    5
+    """
+    def strategy(score, opponent_score):
+        return n
+    return strategy

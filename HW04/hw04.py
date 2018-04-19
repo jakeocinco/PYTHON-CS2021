be89168fe779e49360d6204ac7a9ae075bc8814e
jakeocinco <jakeocinco@me.com>
2018-04-19 15:05:40 -0400

    Finished HW

--- HW04/hw04.py
+++ HW04/hw04.py
@@ -134,109 +134,149 @@ class Link:
 
 
     def __iadd__(self, other):
+        def copyLink(o):
+            if o.rest is ():
+                return Link(o.first)
+            else:
+                return Link(o.first, copyLink(o.rest))
+
+        q = copyLink(other)
+
         s = self
         while True:
             if s.rest is ():
-                s.rest = other
+                s.rest = q
+                return self
+            else:
+                s = s.rest
 
 
 
 
-# Link(5, Link(1))
-s = Link(1, Link(2, Link(3)))
-# s.rest.rest.rest = s
-# # print(has_cycle(s))
-# # # True
-# t = Link(1, Link(2, Link(3)))
-# # print(has_cycle(t))
-# # # False
 
-# class ScaleIterator:
-#     """An iterator the scales elements of the iterable s by a number k.
-#
-#     >>> s = ScaleIterator([1, 5, 2], 5)
-#     >>> list(s)
-#     [5, 25, 10]
-#
-#     >>> m = ScaleIterator(naturals(), 2)
-#     >>> [next(m) for _ in range(5)]
-#     [2, 4, 6, 8, 10]
-#     """
-#     def __init__(self, s, k):
-#         "*** YOUR CODE HERE ***"
-#
-#     def __iter__(self):
-#         return self
-#
-#     def __next__(self):
-#         "*** YOUR CODE HERE ***"
-#
-#
-#
-# def scale(s, k):
-#     """Yield elements of the iterable s scaled by a number k.
-#
-#     >>> s = scale([1, 5, 2], 5)
-#     >>> type(s)
-#     <class 'generator'>
-#     >>> list(s)
-#     [5, 25, 10]
-#
-#     >>> m = scale(naturals(), 2)
-#     >>> [next(m) for _ in range(5)]
-#     [2, 4, 6, 8, 10]
-#     """
-#     "*** YOUR CODE HERE ***"
-#
-#
-# def merge(s0, s1):
-#     """Yield the elements of strictly increasing iterables s0 and s1, removing
-#     repeats. Assume that s0 and s1 have no repeats. You can also assume that s0
-#     and s1 represent infinite sequences.
-#
-#     >>> twos = scale(naturals(), 2)
-#     >>> threes = scale(naturals(), 3)
-#     >>> m = merge(twos, threes)
-#     >>> type(m)
-#     <class 'generator'>
-#     >>> [next(m) for _ in range(10)]
-#     [2, 3, 4, 6, 8, 9, 10, 12, 14, 15]
-#     """
-#     i0, i1 = iter(s0), iter(s1)
-#     e0, e1 = next(i0), next(i1)
-#     "*** YOUR CODE HERE ***"
-#
-#
-#
-# def make_s():
-#     """A generator function that yields all positive integers with only factors
-#     2, 3, and 5.
-#
-#     >>> s = make_s()
-#     >>> type(s)
-#     <class 'generator'>
-#     >>> [next(s) for _ in range(20)]
-#     [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36]
-#     """
-#     "*** YOUR CODE HERE ***"
-#
-#
-# def naturals():
-#     """A generator function that yields the infinite sequence of natural
-#     numbers, starting at 1.
-#
-#     >>> m = naturals()
-#     >>> type(m)
-#     <class 'generator'>
-#     >>> [next(m) for _ in range(10)]
-#     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#     """
-#     i = 1
-#     while True:
-#         yield i
-#         i += 1
-#
-#
+
+class ScaleIterator:
+    """An iterator the scales elements of the iterable s by a number k.
+
+    >>> s = ScaleIterator([1, 5, 2], 5)
+    >>> list(s)
+    [5, 25, 10]
+
+    >>> m = ScaleIterator(naturals(), 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    def __init__(self, s, k):
+        self.s = s
+        self.k = k
+        self.i = -1
+
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.i += 1
+        try:
+            return self.s[self.i] * self.k
+        except:
+            try:
+                return next(self.s) * self.k
+            except:
+                raise StopIteration
+
+
+def scale(s, k):
+    """Yield elements of the iterable s scaled by a number k.
+
+    >>> s = scale([1, 5, 2], 5)
+    >>> type(s)
+    <class 'generator'>
+    >>> list(s)
+    [5, 25, 10]
+
+    >>> m = scale(naturals(), 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    "*** YOUR CODE HERE ***"
+    for i in s:
+        yield (i) * k
+
+def merge(s0, s1):
+    """Yield the elements of strictly increasing iterables s0 and s1, removing
+    repeats. Assume that s0 and s1 have no repeats. You can also assume that s0
+    and s1 represent infinite sequences.
+
+    >>> twos = scale(naturals(), 2)
+    >>> threes = scale(naturals(), 3)
+    >>> m = merge(twos, threes)
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [2, 3, 4, 6, 8, 9, 10, 12, 14, 15]
+    """
+    i0, i1 = iter(s0), iter(s1)
+    e0, e1 = next(i0), next(i1)
+    "*** YOUR CODE HERE ***"
+    while True:
+        if e0 < e1:
+            yield e0
+            e0 = next(i0)
+        elif e0 > e1:
+            yield e1
+            e1 = next(i1)
+        else:
+            yield e1
+            e0 = next(i0)
+            e1 = next(i1)
+
+
+def make_s():
+    """A generator function that yields all positive integers with only factors
+    2, 3, and 5.
+
+    >>> s = make_s()
+    >>> type(s)
+    <class 'generator'>
+    >>> [next(s) for _ in range(20)]
+    [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36]
+    """
+    "*** YOUR CODE HERE ***"
+
+    while True:
+        yield 1
+        s2 = scale(make_s(),2);
+        s3 = scale(make_s(),3);
+        s5 = scale(make_s(),5);
+
+        m = merge(s2,merge(s3,s5))
+        while True:
+            yield next(m)
+
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+twos = scale(naturals(), 2)
+threes = scale(naturals(), 3)
+m = merge(twos, threes)
+print(type(m))
+# <class 'generator'>
+# [next(m) for _ in range(10)]
+# [2, 3, 4, 6, 8, 9, 10, 12, 14, 15]
 # def _test():
 #     import doctest
 #     doctest.testmod()
